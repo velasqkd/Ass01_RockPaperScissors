94d14bbeddd194c6d0310396945ecccab4ba2ce0
velasqkd <velasqkd@mail.uc.edu>
2025-03-02 19:27:20 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        String playAgain;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            String playerAChoice = "";
+            while (!playerAChoice.equals("R") && !playerAChoice.equals("P") && !playerAChoice.equals("S")) {
+                System.out.print("Player A, enter your choice (R/P/S): ");
+                playerAChoice = in.nextLine().toUpperCase();
+                if (!playerAChoice.equals("R") && !playerAChoice.equals("P") && !playerAChoice.equals("S")) {
+                    System.out.println("Invalid input. Please choose R, P, or S.");
+                }
+            }
+
+            String playerBChoice = "";
+            while (!playerBChoice.equals("R") && !playerBChoice.equals("P") && !playerBChoice.equals("S")) {
+                System.out.print("Player B, enter your choice (R/P/S): ");
+                playerBChoice = in.nextLine().toUpperCase();
+                if (!playerBChoice.equals("R") && !playerBChoice.equals("P") && !playerBChoice.equals("S"))
+                {
+                    System.out.println("Invalid input. Please choose R, P, or S.");
+                }
+            }
+
+            System.out.println("Player A chose " + playerAChoice);
+            System.out.println("Player B chose " + playerBChoice);
+
+            if (playerAChoice.equals(playerBChoice)) {
+                System.out.println("It's a Tie!");
+            } else if ((playerAChoice.equals("R") && playerBChoice.equals("S")) ||
+                    (playerAChoice.equals("P") && playerBChoice.equals("R")) ||
+                    (playerAChoice.equals("S") && playerBChoice.equals("P"))) {
+                System.out.println("Player A wins!");
+                if (playerAChoice.equals("R") && playerBChoice.equals("S")) {
+                    System.out.println("Rock breaks Scissors!");
+                } else if (playerAChoice.equals("P") && playerBChoice.equals("R")) {
+                    System.out.println("Paper covers Rock!");
+                } else {
+                    System.out.println("Scissors cuts Paper!");
+                }
+            } else {
+                System.out.println("Player B wins!");
+                if (playerBChoice.equals("R") && playerAChoice.equals("S")) {
+                    System.out.println("Rock breaks Scissors!");
+                } else if (playerBChoice.equals("P") && playerAChoice.equals("R")) {
+                    System.out.println("Paper covers Rock!");
+                } else {
+                    System.out.println("Scissors cuts Paper!");
+                }
+            }
+
+            System.out.print("Do you want to play again? (Y/N): ");
+            playAgain = in.nextLine().toUpperCase();
+
+        } while (playAgain.equals("Y"));
+
+        System.out.println("Thank you for playing!");
     }
-}
